5e0b7d4e2b924255aee9d75d565b67a3edb7775e
Leonid Lachmann <148564629+leoleiden@users.noreply.github.com>
2025-06-26 18:51:47 +0300

    Update task.sql

--- task.sql
+++ task.sql
@@ -1,11 +1,24 @@
 -- Use our database
-USE ShopDB; 
-
--- Some data should be created outside the transaction (here)
+USE ShopDB;
 
 -- Start the transaction 
-START TRANSACTION; 
+START TRANSACTION;
 
--- And some data should be created inside the transaction 
+-- 1. Створити нове замовлення від клієнта з ID = 1, дата = 2023-01-01
+INSERT INTO Orders (CustomerID, Date)
+VALUES (1, '2023-01-01');
 
-COMMIT; 
+-- 2. Отримати ID створеного замовлення
+SET @order_id = LAST_INSERT_ID();
+
+-- 3. Додати товар AwersomeProduct (ID = 1, Count = 1)
+INSERT INTO OrderItems (OrderID, ProductID, Count)
+VALUES (@order_id, 1, 1);
+
+-- 4. Зменшити залишок товару на складі
+UPDATE Products
+SET WarehouseAmount = WarehouseAmount - 1
+WHERE ID = 1;
+
+-- Завершити транзакцію
+COMMIT;
